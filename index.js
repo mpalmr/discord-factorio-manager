8a6545110cae16ae749230d29dca5e20ebe41602
Matthew Palmer <mspalmer91@gmail.com>
2020-02-28 19:51:20 -0500

    login

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('dotenv').config();
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);

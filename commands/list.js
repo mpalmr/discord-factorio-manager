c1b9c43871ea8b0c8e3aa4e97a33b934faa7bdd8
Matthew Palmer <mspalmer91@gmail.com>
2020-02-29 13:16:49 -0500

    column formatting

--- commands/list.js
+++ commands/list.js
@@ -1,8 +1,25 @@
 'use strict';
 
+const columnify = require('columnify');
+
 module.exports = async function listCommand(channel) {
-	channel.send(`
-		Instance
-		========
+	channel.send(`\`\`\`
+${columnify(
+		[
+			{
+				instance: 'Vanilla',
+				status: 'ONLINE',
+			},
+			{
+				instance: 'Vanilla QoL',
+				status: 'OFFLINE',
+			},
+		],
+		{
+			columns: ['instance', 'status'],
+			columnSplitter: '    ',
+		},
+	)}
+\`\`\`
 	`);
 };

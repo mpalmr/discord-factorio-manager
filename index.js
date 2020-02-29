98e2b334a693ff095de3208e0880aac40a7f15cd
Matthew Palmer <mspalmer91@gmail.com>
2020-02-28 22:11:16 -0500

    command parsing

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', ({ content, channel }) => {
-	if (content === '!ping') channel.send('Pong.');
-});
+const commandPattern = new RegExp(`^\\s*${process.env.COMMAND_PREFIX}`);
+function parseMessage(handleCommand) {
+	return ({ channel, content }) => {
+		if (!commandPattern.test(content)) return null;
+		const [commandName, ...args] = content.trim().slice(1).split(/\s+/g);
+		return handleCommand(channel, {
+			name: commandName.toLowerCase(),
+			args,
+		});
+	};
+}
+
+client.on('message', parseMessage((channel, command) => {
+	if (command.name === 'ping') channel.send('Pong.');
+}));
 
 client.login(process.env.DISCORD_BOT_TOKEN);

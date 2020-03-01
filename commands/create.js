7ba958cc7e52417b0fa7ffc2c88a1b691ee6161b
Matthew Palmer <mspalmer91@gmail.com>
2020-03-01 16:09:01 -0500

    finish create command

--- commands/create.js
+++ commands/create.js
@@ -1,12 +1,36 @@
 'use strict';
 
+const path = require('path');
+const fs = require('fs').promises;
+
 module.exports = async function createCommand({ channel, docker }, name) {
-	// Validation
 	if (!(/^[a-z\d-]{4,}$/.test(name))) {
 		channel.send(
 			'Invalid name parameter. Must contain 4 or more alphanumeric characters or dashes.',
 		);
 		return null;
 	}
-	return null;
+
+	const volumePath = path.resolve(`volumes/${name}`);
+	return fs.mkdir(volumePath)
+		.then(() => docker.command(`build -t ${process.env.DOCKER_TAG} .`))
+		.then(() => docker.command(`run -dP \
+			-v ${volumePath}:/factorio \
+			--name ${name} \
+			--restart=always \
+			factoriotools/factorio
+		`))
+		.then(({ containerId }) => docker.command(`stop ${containerId}`))
+		.then(result => {
+			console.log(result);
+			channel.send('Game created.');
+			return result;
+		})
+		.catch(error => {
+			if (error.code) {
+				channel.send('Game by that name already exists.');
+				return null;
+			}
+			return fs.rmdir(volumePath).then(() => Promise.reject(error));
+		});
 };
